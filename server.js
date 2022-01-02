39d7b57e12baa23b7374be47925eb0e290ec97eb
Jolanta <jolka.kantor@o2.pl>
2022-01-02 11:07:27 +0100

    commit z dodanym get-em /data

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
+obiekt = {imie: "Imię", nazwisko: "Nazwisko", klasa: "klasa", grupa: "grupa"}
+
+
 app.get("/", function (req, res) {
     res.send("<h1>first app on heroku! - kolejna zmiana w pliku</h1>")
+})
+
+app.get("/data", function (req, res) {
+    res.send(obiekt)
 })
 
 app.listen(PORT, function () {
